ee10b79e031d9a89b956c0d1fc99dc48ac1ab1ac
Naga Vamsi <nagavamsivadapalli99@gmail.com>
2024-12-21 15:51:58 +0530

    second commit

--- itemRoutes.js
+++ itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Item = require('./Item');
+const Item = require('./Items');
 const Category = require('./Category');
 
 const router = express.Router();
